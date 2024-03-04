1a7c91132e6f1426ba93342f42821cc703c6ee41
pawel <pawel.marcisz1@gmail.com>
2024-03-04 18:49:13 +0100

    Add mainpage functions

--- GitQuiz/GitQuiz/MainPage.xaml.cs
+++ GitQuiz/GitQuiz/MainPage.xaml.cs
@@ -13,6 +13,35 @@ namespace GitQuiz
         public MainPage()
         {
             InitializeComponent();
+
+        }
+
+        private void FinishQuiz()
+        {
+            double totalTime = times.Sum();
+            SaveResult(userNameEntry.Text, totalTime, currentScore);
+            DisplayFinalResults(totalTime);
+        }
+
+        private async void ViewScoresClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ScoresPage());
+        }
+
+        private void DisplayFinalResults(double totalTime)
+        {
+            finishGameView.IsVisible = true;
+            gameView.IsVisible = false;
+            questionLabel.IsVisible = false;
+            answerEntry.IsVisible = false;
+            submitAnswerButton.IsVisible = false;
+            resultPoints.Text = curentScore.ToString();
+            resultTime.Text = totalTime.ToString("F2");
+        }
+
+        private void SaveResult(string userName, double totalTime, int score)
+        {
+            App.Database.SaveResultAsync(new UserResult(userName, totalTime, score));
         }
     }
 }
